15d313f8e4e8ab0e582b1a75798e62b989a5cd7c
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-08 03:18:47 +0000

    asdf

--- api-crud/src/Components/Add.jsx
+++ api-crud/src/Components/Add.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Add = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const postdata = (e) => {
+        let data = { name, email };
+        console.log(data)
+        e.preventDefault();
+        fetch("http://localhost:3004/userdata", {
+            method: "POST",
+            header: {
+                "Accept": "application/json",
+                "Application/type": "application/json"
+            },
+            body: JSON.stringify({ name, email })
+        }).then((response) => { console.log(response) })
+            .catch((error) => {
+                console.log(error + "")
+            })
+    }
     return (
         <>
             <div className="container">
@@ -9,7 +29,11 @@ const Add = () => {
 
                 <div className="row">
                     <div className="col-12 col-sm-8 col-md-10">
-
+                        <form action="" method="post">
+                            <input type="text" name="" id="" className="form-control" placeholder="" value={name} onChange={(e) => { setName(e.target.value) }} />
+                            <input type="text" name="" id="" className="form-control" placeholder="" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                            <button className="btn btn-info" onClick={postdata}>Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- api-crud/src/Components/Index.jsx
+++ api-crud/src/Components/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavLink } from 'react-router-dom';
@@ -11,12 +11,15 @@ const Index = () => {
         const users = await data.json();
         setUser(users);
     }
+    useEffect(() => {
+        getUsers();
+    }, []);
     return (
         <div>
             <div className="container">
-                <h1 className="text-center fw-bold display-4">All Users</h1>
+                <h1 className="text-center fw-bold display-4 mb-3">All Users</h1>
                 <div className="row">
-                    <div className="col-12 col-sm-12 col-md-10">
+                    <div className="col-12 col-sm-12 col-md-12 m-auto">
                         <table className="table table-bordered table-striped">
                             <tr>
                                 <th>Id</th>
@@ -33,8 +36,8 @@ const Index = () => {
                                                 <th>{user.id}</th>
                                                 <th>{user.name}</th>
                                                 <th>{user.email}</th>
-                                                <th><NavLink to="/edit/{user.id}" className="btn btn-success">Edit</NavLink></th>
-                                                <th><NavLink to="/delete/{user.id}" className="btn btn-danger">Delete</NavLink></th>
+                                                <th><button onclick={ } className="btn bg-success text-white">Edit</button></th>
+                                                <th><button onclick={ } className="btn bg-danger text-white">Delete</button></th>
                                             </tr>
                                         </>
                                     )
