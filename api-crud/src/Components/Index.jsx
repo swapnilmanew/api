2fab7da75f37cd703f401a684c44ed170db09a08
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-08 15:15:57 +0000

    promise

--- api-crud/src/Components/Index.jsx
+++ api-crud/src/Components/Index.jsx
@@ -19,24 +19,33 @@ const Index = () => {
     const [email, setEmail] = useState("");
 
 
-    const editData = (id) => {
-    //     fetch(`http://localhost:3004/userdata/${id}`, {
-    //         method: 'PUT',
-    //         headers:
-    //         {
-    //             "Content-Type": "application/json; Charset=UTF-8"
-    //         },
-    //         body: JSON.stringify({
+    const editData = async (id) => {
+        console.log(id);
+        const api = await fetch(`http://localhost:3004/userdata/${id}`);
+        const data = await api.json();
+        setName(data.name)
+        setEmail(data.email);
 
-    //         })
-    //     })
-    //         .then((res) => console.log(res))
-    //         .catch((err) => console.log(err))
-    // }
-
-    const deleteData = (id) => {
-        alert(id)
     }
+
+    const updateData = (e) => {
+        e.preventDefault();
+        console.log(name, email);
+
+        // fetch(`http://localhost:3004/userdata/${id}`, {
+        //     method: 'PUT',
+        //     headers:
+        //     {
+        //         "Content-Type": "application/json; Charset=UTF-8"
+        //     },
+        //     body: JSON.stringify({
+
+        //     })
+        // })
+        //     .then((res) => console.log(res))
+        //     .catch((err) => console.log(err))
+    }
+
     return (
         <div>
             <div className="container">
@@ -60,7 +69,7 @@ const Index = () => {
                                                 <th>{user.name}</th>
                                                 <th>{user.email}</th>
                                                 <th><button onClick={() => editData(user.id)} className="btn bg-success text-white">Edit</button></th>
-                                                <th><button onClick={() => deleteData(user.id)} className="btn bg-danger text-white">Delete</button></th>
+                                                {/* <th><button onClick={() => deleteData(user.id)} className="btn bg-danger text-white">Delete</button></th> */}
                                             </tr>
                                         </>
                                     )
@@ -72,7 +81,7 @@ const Index = () => {
                         <form action="">
                             <input type="text" name="" id="" className="form-control my-3" onChange={(e) => setName(e.target.value)} value={name} placeholder="Name" />
                             <input type="text" name="" id="" className="form-control my-3" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email" />
-                            <button className="btn btn-info">Edit</button>
+                            <button className="btn btn-info" onClick={updateData}>Edit</button>
                         </form>
                     </div>
                 </div>
@@ -81,4 +90,4 @@ const Index = () => {
     )
 }
 
-export default Index
+export default Index;
